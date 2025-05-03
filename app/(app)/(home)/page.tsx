f9049c11edcd9eab1bb2602ce43c6c42f2402539
clevermiraz <mirazhs@pm.me>
2025-05-03 21:11:44 +0600

    Authentication Done 🎉

--- app/(app)/(home)/page.tsx
+++ app/(app)/(home)/page.tsx
@@ -1,3 +1,11 @@
+"use client";
+
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+
 export default function Home() {
-  return <div className="text-red-500">Hello World</div>;
+  const trpc = useTRPC();
+  const session = useQuery(trpc.auth.session.queryOptions());
+
+  return <div className="text-red-500">{JSON.stringify(session.data?.user, null, 2)}</div>;
 }
